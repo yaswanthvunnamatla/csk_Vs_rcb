92e679dd61b9708d8ff6dab7193c57707b2ea068
yaswanthvunnamatla <unnamatla999@gmail.com>
2024-01-23 07:01:51 +0000

    Super Over League

---
+++ index.js
@@ -0,0 +1,13 @@
+const element = (
+  <div className='container'>
+    <h1 className='heading'>Super Over League</h1>
+    <div className='d-flex flex-row image-1'>
+      <img src='https://assets.ccbp.in/frontend/react-js/rcb-img.png' />
+    </div>
+    <div className='d-flex flex-row image-2'>
+      <img src='https://assets.ccbp.in/frontend/react-js/csk-img.png' />
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
